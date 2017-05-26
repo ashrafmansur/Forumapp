92c88b4cf0e9106d379cdfb81bb47b7a5c6f1aa5
ashrafmansur <ashraf261090@gmail.com>
2017-05-26 14:50:47 +0800

    Add edit delete features to topics and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-	
+	before_action :set_comment, only: []
+
   def new
     @topic = Topic.find(params[:topic_id])
     @comment = @topic.comments.build
@@ -7,15 +8,46 @@ class CommentsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-
     @comment = @topic.comments.build(comment_params)
     @comment.user = current_user
-    if @comment.save
     
+    if @comment.save
+      redirect_to topic_path(@topic)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update_attributes(comment_params)
+      redirect_to topic_path(@topic)
+    else
+      render :edit
+    end
+
+  end
+
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
     redirect_to topic_path(@topic)
   end
 
   private
+
+  def set_comment
+    
+  end
 
   def comment_params
     params.require(:comment).permit(:body)  
