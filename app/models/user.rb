1b8ae755d705ccd3917a995f743fdbf1483158a2
ashrafmansur <ashraf261090@gmail.com>
2017-05-26 15:12:50 +0800

    Fix user edit bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  validates_presence_of :firstname, :lastname, :email, :password
+  validates_presence_of :firstname, :lastname, :email
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
